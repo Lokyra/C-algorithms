2ae85f71f15abc900f86e06a024876389307fe2a
ayoub <ayoubousbia02@gmail.com>
2023-05-25 20:53:02 +0200

    add recursive version of binary search

--- array-adt/searchOnArray.c
+++ array-adt/searchOnArray.c
@@ -13,6 +13,49 @@ int LinearSearch(struct Array *arr, int key) {
     return -1;
 }
 
+int BinarySearch(struct Array *arr, int key) {
+    int l = 0;
+    int h = arr->length - 1;
+    int mid = 0;
+
+    while (l <= h) {
+        mid = (l + h) / 2;
+
+        if (arr->A[mid] == key)
+            return mid;
+
+        else if (arr ->A[mid] < key) {
+            h = mid - 1;
+        } else {
+            l = mid + 1;
+    
+        }
+    }
+
+    return -1;
+}
+
+int BinarySearchRec(int arr[], int l, int h, int key)
+{
+    int mid = 0;
+
+    if (l <= h) {
+        mid = (l + h) / 2;
+
+        if (key == arr[mid])
+            return mid;
+
+        else if (key < arr[mid]) {
+            return BinarySearchRec(arr, l, mid-1, key);
+        } else {
+            return BinarySearchRec(arr, l, mid+1, key);
+        }
+    }
+
+    return -1;
+
+}
+
 int main()
 {
     return 0;
