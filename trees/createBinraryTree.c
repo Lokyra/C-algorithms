9849320f2781b4fde125bf232456154380195d5d
ayoub <ayoubousbia02@gmail.com>
2023-11-13 06:39:25 +0100

    add queue header and start creating binary tree

---
+++ trees/createBinraryTree.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "Queue.h"
+
+
+struct Node *root=NULL;
+
+void create() {
+
+  struct Node *p, *t;
+  int x;
+  struct Queue q;
+  create(&q,100);
+
+  printf("enter root value");
+  scanf("%d", &x);
+}
+
+int main() {
+  return 0;
+}
